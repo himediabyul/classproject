f95f3b20ae01b8412a921b4dd89f4fa3810320c6
himediabyul <himedia.byul@gmail.com>
2022-09-19 16:01:57 +0900

    챕터5 과제
    Member 인스턴스

---
+++ JAVA project/First Java/src/chapter05/overload/Member.java
@@ -0,0 +1,38 @@
+package chapter05.overload;
+
+public class Member {
+	
+	String name;
+	String number;
+	String major;
+	String grade;
+	String email;
+	int birthDay;
+	String address;
+	
+	
+		
+
+
+	Member(String name, String number, String major, String grade, String email, int birthDay, String address){
+	
+	this.name=name;
+	this.number = number;
+	this.major = major;
+	this.grade = grade;
+	this.email = email;
+	this.birthDay = birthDay;
+	this.address = address;
+	}
+	
+	void showInfo(){
+		System.out.println("이름 : " + name);
+		System.out.println("전화번호 : " + number );
+		System.out.println("전공 : " + major );
+		System.out.println("학년 : " + grade );
+		System.out.println("email : " + email );
+		System.out.println("생일 : " + birthDay );
+		System.out.println("주소 : " + address );
+		
+	}
+}
