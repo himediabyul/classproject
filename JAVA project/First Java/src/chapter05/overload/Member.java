e63bf1c9fe52f1fe98285bc8d9dd000194d44469
himediabyul <himedia.byul@gmail.com>
2022-09-19 17:10:57 +0900

    Update Member.java

--- JAVA project/First Java/src/chapter05/overload/Member.java
+++ JAVA project/First Java/src/chapter05/overload/Member.java
@@ -1,5 +1,17 @@
 package chapter05.overload;
 
+
+/*
+ 	 Member 클래스에는 아래 요구사항에 맞는 변수와 메소드를 정의하고, main()메소드 에 해당 메소드를 호출해서 결과를 확인해보세요.
+ 	 단 인스턴스의 생성은 생성자를 이용해서 초기화하는 코드를 작성해 봅시다. 
+	① 아래의 데이터를 저장 이름, 전화번호, 전공, 학년, email, 생일, 주소
+	② 위에서 정의한 정보를 출력하는 메소드 정의
+	③ 모든 정보를 저장하도록 하는 생성자와 생일과 주소는 저장하지 않을 수 있는 생성자를 정의
+	④ main() 메소드에서 두 가지 생성자를 이용해서 인스턴스 생성하고 출력 메소드를 통해 저장된 데이터 출력 
+ */
+
+
+
 public class Member {
 	
 	String name;
@@ -18,13 +30,13 @@ public class Member {
 
 	Member(String name, String number, String major, String grade, String email, int birthDay, String address){
 	
-	this.name=name;
-	this.number = number;
-	this.major = major;
-	this.grade = grade;
-	this.email = email;
-	this.birthDay = birthDay;
-	this.address = address;
+		this.name=name;
+		this.number = number;
+		this.major = major;
+		this.grade = grade;
+		this.email = email;
+		this.birthDay = birthDay;
+		this.address = address;
 	}
 	
 	Member(String name, String number, String major, String grade, String email){
@@ -34,10 +46,12 @@ public class Member {
 		this.major = major;
 		this.grade = grade;
 		this.email = email;
+		// this(name, number, major, grade, email, 0, null);
 	}
 	
 	void showInfo(){
-				
+		
+		
 		System.out.println("멤버소개");
 		System.out.println("이름 : " + name);
 		System.out.println("전화번호 : " + number );
@@ -45,18 +59,19 @@ public class Member {
 		System.out.println("학년 : " + grade );
 		System.out.println("email : " + email );
 		
+		
 		if(birthDay==0) {
-			return;
-					}
+		return; }
 		
 		System.out.println("생일 : " + birthDay );
 		System.out.println("주소 : " + address );
 		
 		
 		
+		
 	}
 
-	
+
 
 		
 }
