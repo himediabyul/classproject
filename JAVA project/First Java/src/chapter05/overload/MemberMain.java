fd450c8cff1e4f99ca6513e9df9dd88a0988883d
himediabyul <himedia.byul@gmail.com>
2022-09-19 16:14:23 +0900

    Update MemberMain.java

--- JAVA project/First Java/src/chapter05/overload/MemberMain.java
+++ JAVA project/First Java/src/chapter05/overload/MemberMain.java
@@ -7,8 +7,11 @@ public class MemberMain {
 		Member mb1 = new Member("이샛별", "010-****-****", "Trade", "졸업", "himedia.byul@gmail.com", 930213, "***");
 		mb1.showInfo();
 		
+		System.out.println();
+		
 		Member mb2 = new Member("이샛별", "010-****-****", "Trade", "졸업", "himedia.byul@gmail.com");
-		mb2.showInfo();
+		mb2.showInfo2();
+		
 	}
 
 }
