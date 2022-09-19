186303facbbc004687a42682dbba66021cc91b96
himediabyul <himedia.byul@gmail.com>
2022-09-19 16:02:17 +0900

    챕터5 과제
    Member main

---
+++ JAVA project/First Java/src/chapter05/overload/MemberMain.java
@@ -0,0 +1,11 @@
+package chapter05.overload;
+
+public class MemberMain {
+	
+	public static void main(String[] args) {
+		
+		Member mb1 = new Member("이샛별", "010-****-****", "Trade", "졸업", "himedia.byul@gmail.com", 930213, "***");
+		mb1.showInfo();
+	}
+
+}
