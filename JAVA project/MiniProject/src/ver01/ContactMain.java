bf041dd51889ad9ad5900f3bb9de729a6bbdf527
himediabyul <himedia.byul@gmail.com>
2022-09-20 18:18:28 +0900

    Create ContactMain.java

---
+++ JAVA project/MiniProject/src/ver01/ContactMain.java
@@ -0,0 +1,46 @@
+package ver01;
+
+import java.util.Scanner;
+
+public class ContactMain {
+	
+	// main()메소드를 정의합니다.
+	public static void main(String[] args) {
+
+			
+		
+		Scanner in = new Scanner(System.in);
+		
+		while (true) {
+	System.out.println("입력 1, 종료 0");
+	String input = in.nextLine();
+		
+			if(input.equals("0") ) {
+				break;
+			}
+
+		System.out.println("이름을 입력해주세요>>> ");
+		String name = in.nextLine();
+		
+		System.out.println("전화번호를 입력해주세요>>> ");
+		String phoneNumber = in.nextLine();
+		
+		System.out.println("이메일을 입력해주세요>>> ");
+		String email = in.nextLine();
+		
+		System.out.println("주소를 입력해주세요>>> ");
+		String address = in.nextLine();
+		
+		System.out.println("생일을 입력해주세요>>> ");
+		String birthDay = in.nextLine();
+		
+		System.out.println("그룹을 입력해주세요>>> ");
+		String group = in.nextLine();
+	
+	Contact m1 = new Contact(name, phoneNumber, email, address, birthDay, group);
+	m1.showContact();
+	
+	
+			}
+	}
+	}
