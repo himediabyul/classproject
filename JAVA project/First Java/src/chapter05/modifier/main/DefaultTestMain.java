baa90ba34a7d160f7938744405e5f36b289267ec
himediabyul <himedia.byul@gmail.com>
2022-09-22 09:07:21 +0900

    Create DefaultTestMain.java

---
+++ JAVA project/First Java/src/chapter05/modifier/main/DefaultTestMain.java
@@ -0,0 +1,17 @@
+package chapter05.modifier.main;
+
+import chapter05.modifier.DefaultTest;
+
+public class DefaultTestMain {
+	
+	public static void main(String[] args) {
+		
+		DefaultTest dt = new DefaultTest();
+		
+		// dt.age = 30; 다른패키지 이기때문에 안됨
+		
+	}
+	
+	
+
+}
