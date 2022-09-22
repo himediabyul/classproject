8fab7c945c818c4ad3bacd08ca143e06094e0ed5
himediabyul <himedia.byul@gmail.com>
2022-09-22 09:07:58 +0900

    Create StaticVariable.java

---
+++ JAVA project/First Java/src/chapter05/StaticVariable.java
@@ -0,0 +1,18 @@
+package chapter05;
+
+public class StaticVariable {
+	
+	static int cv;
+	int iv;
+	
+	//상수 : 변하지 않는 수
+	//상수는 대문자로
+	static final float PI = 3.14f;
+	
+	public static void main(String[] args) {
+		System.out.println(cv);
+		//System.out.println(iv);
+		
+	}
+
+}
