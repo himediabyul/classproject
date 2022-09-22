37da1f460474e5b7da1778fd720187bac78ae95f
himediabyul <himedia.byul@gmail.com>
2022-09-22 09:07:42 +0900

    Create SmartPhoneMain.java

---
+++ JAVA project/First Java/src/chapter05/overload/SmartPhoneMain.java
@@ -0,0 +1,30 @@
+package chapter05.overload;
+
+public class SmartPhoneMain {
+	
+	public static void main(String[] args) {
+		
+			//SmartPhone 인스턴스 생성
+		SmartPhone sp1 = new SmartPhone("RED", 4.7f, "삼성", 5 );
+		
+		sp1.showInfo();
+		
+		SmartPhone sp2 = new SmartPhone("GOLD", 5.0f, "애플", 10 );
+		
+		sp2.showInfo();
+		
+		SmartPhone sp3 = new SmartPhone();
+		
+		//SmartPhone sp3
+		//new SmartPhone
+		//SmartPhone()
+		
+		SmartPhone sp4 = new SmartPhone("apple", 6f);
+		sp4.showInfo();
+		
+		SmartPhone sp5 = new SmartPhone("LG");
+		sp5.showInfo();
+		
+	}
+
+}
