1020353a6e3d89a7c3c8876b777a0a8e8fddce7a
himediabyul <himedia.byul@gmail.com>
2022-09-22 09:07:24 +0900

    Create PrivateTest.java

---
+++ JAVA project/First Java/src/chapter05/modifier/PrivateTest.java
@@ -0,0 +1,19 @@
+package chapter05.modifier;
+
+public class PrivateTest {
+	
+	private String name;
+	private int age;
+	public PrivateTest() {
+		this.name = "이샛별";
+		this.age = 30;
+	}
+	
+	public void showInfo() {
+		System.out.println("이름 : " + this.name);
+		System.out.println("나이 : " + this.age);
+	}
+	
+	
+
+}
