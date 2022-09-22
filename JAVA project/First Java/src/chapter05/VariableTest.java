cd58bfd3bce9da0aecb37031c1b0703d082db5ef
himediabyul <himedia.byul@gmail.com>
2022-09-22 09:08:05 +0900

    Create VariableTest.java

---
+++ JAVA project/First Java/src/chapter05/VariableTest.java
@@ -0,0 +1,22 @@
+package chapter05;
+
+import java.util.Calendar;
+
+public class VariableTest {
+	
+	public static void main(String[] args) {
+		
+		int iv = StaticVariable.cv;
+		int iv2;
+		
+		//static int cv;
+		
+		//클래스 변수를 참조할 때는 클래스이름.변수이름
+		System.out.println(StaticVariable.cv);
+		System.out.println(StaticVariable.PI);
+		
+		
+		
+	}
+
+}
