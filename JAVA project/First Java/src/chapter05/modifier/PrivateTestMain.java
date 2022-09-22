838b814b9dc0479e5a7969bdb6d20d51f6f7c64b
himediabyul <himedia.byul@gmail.com>
2022-09-22 09:07:27 +0900

    Create PrivateTestMain.java

---
+++ JAVA project/First Java/src/chapter05/modifier/PrivateTestMain.java
@@ -0,0 +1,24 @@
+package chapter05.modifier;
+
+public class PrivateTestMain {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		PrivateTest pt = new PrivateTest();
+		
+		pt.showInfo();
+		
+		
+//		System.out.println(pt.name);
+//		System.out.println(pt.age);
+		
+//		pt.age = 100;
+		
+//		System.out.println(pt.age);
+		
+		
+	}
+
+}
