509204b30ea1d08559e31f3e3039f2d86c4ab2bf
himediabyul <himedia.byul@gmail.com>
2022-09-26 18:03:32 +0900

    Create NewClass.java

---
+++ JAVA project/First Java/src/chapter08/NewClass.java
@@ -0,0 +1,22 @@
+package chapter08;
+
+public class NewClass extends AbstractClass {
+	
+	String nickname;
+
+	//추상클래스를 상소갛고 추상메소드를 구현하지 않으면
+	// 새로운 클래스도 추상 클래스가 된다
+	// NewClass도 인스턴스 생성불가
+	// 인스턴스 생성을 목적으로 한다면 추상메소드를 오버라이딩해서 완성시켜야한다
+	
+	//추상메소드 : 메소드의 정의만 하고 처리부는 완성하지 않은 메소드
+	//추상메소드를 오버라이딩을 통해 완성시킨다
+	
+	@Override
+	void abstractMethod() {
+		System.out.println("추상메소드 오버라이딩하여 완성한 메소드");
+	};
+	
+
+
+}
