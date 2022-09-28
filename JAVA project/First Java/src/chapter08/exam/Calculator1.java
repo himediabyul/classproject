79f075910354dae68de43ef9c482f1c8004e481b
himediabyul <himedia.byul@gmail.com>
2022-09-28 17:51:41 +0900

    Create Calculator1.java

---
+++ JAVA project/First Java/src/chapter08/exam/Calculator1.java
@@ -0,0 +1,14 @@
+package chapter08.exam;
+
+public interface Calculator1 {
+	
+	long add(long n1, long n2);
+	
+	long substract(long n1, long n2);
+	
+	long multiply(long n1, long n2);
+	
+	double divide(double n1, double n2);
+	
+
+}
