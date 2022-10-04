ce21d2658daebfa37b2a94b215bede64a3e2faf0
himediabyul <himedia.byul@gmail.com>
2022-10-04 09:12:06 +0900

    Create SmartPhonePracticeMain.java

---
+++ JAVA project/MiniProject/src/ver02/SmartPhonePracticeMain.java
@@ -0,0 +1,15 @@
+package ver02;
+
+public class SmartPhonePracticeMain {
+	
+	public static void main(String[] args) {
+		
+		SmartPhonePractice smartPhone = SmartPhonePractice.getInstance();
+		
+		
+		smartPhone.saveData();
+		
+		
+	}
+
+}
