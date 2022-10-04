31eb68210841dab8389dab815359f4392626768c
himediabyul <himedia.byul@gmail.com>
2022-10-04 12:02:58 +0900

    Create SmartPhoneMain.java

---
+++ JAVA project/MiniProject/src/ver02/practice/SmartPhoneMain.java
@@ -0,0 +1,15 @@
+package ver02.practice;
+
+public class SmartPhoneMain {
+	
+	public static void main(String[] args) {
+		
+		SmartPhone smartPhone = SmartPhone.getInstance();
+		
+		
+		smartPhone.saveData();
+		
+		
+	}
+
+}
