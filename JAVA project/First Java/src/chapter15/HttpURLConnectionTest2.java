740839083a19bee63c8c122dd933d975474bdf1c
himediabyul <himedia.byul@gmail.com>
2022-10-11 16:12:35 +0900

    Create HttpURLConnectionTest2.java

---
+++ JAVA project/First Java/src/chapter15/HttpURLConnectionTest2.java
@@ -0,0 +1,33 @@
+package chapter15;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class HttpURLConnectionTest2 {
+	
+	public static void main(String[] args) {
+		
+		String urlStr = "https://www.google.com";
+		
+		try {
+			URL url = new URL(urlStr);
+		
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				
+				for(int i =1; i<=8; i++) {
+					System.out.println(connection.getHeaderField(i) + " = " + connection.getHeaderField(i));
+					
+				}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		
+	}
+
+}
+}
