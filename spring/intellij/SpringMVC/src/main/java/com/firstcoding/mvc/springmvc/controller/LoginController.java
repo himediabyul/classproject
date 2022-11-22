da95861538a2f960454d0f5eef7e2314bfe70f98
himedia.byul <himedia.byul@gmail.com>
2022-11-22 18:01:12 +0900

    SpringMVC

--- spring/intellij/SpringMVC/src/main/java/com/firstcoding/mvc/springmvc/controller/LoginController.java
+++ spring/intellij/SpringMVC/src/main/java/com/firstcoding/mvc/springmvc/controller/LoginController.java
@@ -1,6 +1,6 @@
 package com.firstcoding.mvc.springmvc.controller;
 
-import com.firstcoding.mvc.springmvc.model.LoginRequest;
+import com.firstcoding.mvc.springmvc.domain.LoginRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,46 +10,57 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
-@Log4j2
 @Controller
 @RequestMapping("/login")
+@Log4j2
 public class LoginController {
 
-//    @RequestMapping(method = RequestMethod.GET)
+    //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public String getLoginForm(Model model){
-        model.addAttribute("msg", "아이디와 패스워드를 반드시 입력하세요");
-        return "login/form"; // /WEB-INF/views/login/form.jsp
+        model.addAttribute("msg", "아이디와 페스워드를 반드시 입력하세요.");
+        return "login/form";    // /WEB-INF/views/login/form.jsp
     }
 
-//    @RequestMapping(method = RequestMethod.POST)
+    //@RequestMapping(method = RequestMethod.POST)
     @PostMapping
-    public String login(String uid, String pw, String p,// @RequestParam(value = "p", required = true) String userId
-                        @RequestParam(value = "p", defaultValue = "1") String page,
-                        @ModelAttribute("req") LoginRequest loginRequest,
-                        @RequestParam Map paramMap,
-                        HttpServletRequest request,
-                        HttpServletResponse response){
+    public String login(
+            String uid,
+            String pw,
+            String p,
+            //@RequestParam(value = "p", required = true) String page
+            @RequestParam(value = "p", defaultValue = "1") int page,
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @ModelAttribute("req") LoginRequest loginRequest,
+            @RequestParam Map paramMap
+    ){
 
-        String uid2 = request.getParameter("uid");
+        log.info(request.getSession().getServletContext().getRealPath("/css"));
+
+
+        String uId2 = request.getParameter("uid");
         String pw2 = request.getParameter("pw");
 
         log.info("uid => " + uid);
         log.info("pw => " + pw);
-        log.info("p => " + page);
         log.info("p => " + p);
-        log.info("uid2 => " + uid2);
-        log.info("pw2 => " + pw2);
+        log.info("p => " + page);
+        log.info("uid2 => " + uId2);
+        log.info("pw => " + pw2);
         log.info("loginRequest => " + loginRequest);
-        log.info("Map => " + paramMap);
+        log.info("paramMap => " + paramMap);
 
 
-        return "login/login";  // "redirect:/index";
+        request.getSession().setAttribute("loginInfo", uid);
+
+
+        //return "login/login";    // "redirect:/index"
+        return "redirect:/mypage/mypage1";
     }
 
-    @GetMapping("/info") // http://localhost:8080/login/info
+    @GetMapping("/info")   // http://localhost:8080/login/info
     public String info(){
         return "login/info";
     }
-
 }
