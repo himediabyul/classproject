feb2da96fddb98ce12ee359ca0c08e911ef42d97
himedia.byul <himedia.byul@gmail.com>
2022-12-13 18:03:51 +0900

    SpringBoot board

--- spring/Boot/SpringBootBoard/src/main/java/com/app/board/controller/board/ReplyRestController.java
+++ spring/Boot/SpringBootBoard/src/main/java/com/app/board/controller/board/ReplyRestController.java
@@ -1,9 +1,7 @@
 package com.app.board.controller.board;
 
 import com.app.board.domain.ReplyDTO;
-import com.app.board.service.ReplyInsertService;
-import com.app.board.service.ReplyListService;
-import com.app.board.service.ReplyReadService;
+import com.app.board.service.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,6 +25,12 @@ public class ReplyRestController {
 
     @Autowired
     private ReplyReadService readService;
+
+    @Autowired
+    private ReplyEditService replyEditService;
+
+    @Autowired
+    private ReplyDeleteService replyDeleteService;
 
     // get  /reply{bno} => list 전달
     @GetMapping(value="/{bno}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -54,8 +58,24 @@ public class ReplyRestController {
         return new ResponseEntity<>(readService.selectByRno(replyDTO.getRno()), HttpStatus.OK);
     }
 
-    // put  /reply/{rno} => reply 변경 객체
-    // delete /reply/{rno} => 0 or 1, ok or fail
+    // put  /reply/{rno} => reply
+    @PutMapping("/{rno}")
+    public ResponseEntity<Integer> editReply(
+            @RequestBody ReplyDTO replyDTO,
+            @PathVariable("rno") int rno
+    ){
+        replyDTO.setRno(rno);
+        return new ResponseEntity<>(replyEditService.updateReply(replyDTO), HttpStatus.OK);
+    }
+
+    // delete /reply/{rno} => 0 / 1, success, fail
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<Integer> delete(
+            @PathVariable("rno") int rno
+    ){
+        return new ResponseEntity<>(replyDeleteService.deleteByRno(rno), HttpStatus.OK);
+    }
+
 
 
 }
--- spring/Boot/SpringBootBoard/src/main/java/com/app/board/mapper/ReplyMapper.java
+++ spring/Boot/SpringBootBoard/src/main/java/com/app/board/mapper/ReplyMapper.java
@@ -1,10 +1,7 @@
 package com.app.board.mapper;
 
 import com.app.board.domain.ReplyDTO;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -21,6 +18,13 @@ public interface ReplyMapper {
     @Insert("insert into tbl_reply (bno, reply, replyer) values (#{bno}, #{reply}, #{replyer})")
     @Options(useGeneratedKeys = true, keyProperty = "rno", keyColumn = "rno")
     int insertReply(ReplyDTO replyDTO);
+
+    @Delete("delete from tbl_reply where rno=#{rno}")
+    int deleteByRno(int rno);
+
+    @Update("update tbl_reply set bno=#{bno}, reply=#{reply}, replyer=#{replyer}, replydate=#{replydate}, updatedate=now() where rno=#{rno} ")
+    int updateReply(ReplyDTO replyDTO);
+
 
 
 }
